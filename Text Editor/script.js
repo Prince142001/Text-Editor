cb6372a9b0a365b2b73512507bc51c0f2d128c3c
Prince Vishwakarma <83593184+Prince142001@users.noreply.github.com>
2022-08-09 12:43:33 +0530

    Add files via upload

--- Text Editor/script.js
+++ Text Editor/script.js
@@ -1,22 +1,38 @@
-let userInput=document.getElementById("getText");
-let showResult=document.getElementById("showText");
+let userInput = document.getElementById("getText");
+let showResult = document.getElementById("showText");
+let btnClick = document.querySelectorAll('.btn');
+
+// ----------------------------- If the text area is blank then show alert box ------------------------------
+
+for (let i = 0; i < btnClick.length; i++) {
+    btnClick[i].addEventListener("click", textAreaBlank);
+
+    function textAreaBlank() {
+        if (userInput.value == "") {
+            alert(`
+            Textarea is blank...
+            To do any event on this button please fill the textare..
+            `)
+        }
+    }
+}
 
 // ----------------------------- Update Text in next TextArea ------------------------------
-userInput.addEventListener("input",updateText);
+userInput.addEventListener("input", updateText);
 
 function updateText(e) {
-    showResult.textContent=e.target.value;
+    showResult.textContent = e.target.value;
 }
 
 // ----------------------------- Make Bold ------------------------------
-document.querySelector('.makeBold').addEventListener("click",makeBold);
+document.querySelector('.makeBold').addEventListener("click", makeBold);
 
 function makeBold() {
     showResult.classList.toggle('makeBold')
 }
 
 // ----------------------------- Make Italic ------------------------------
-document.querySelector('.makeItalic').addEventListener("click",makeItalic);
+document.querySelector('.makeItalic').addEventListener("click", makeItalic);
 
 function makeItalic() {
     // showResult.style.fontStyle='italic';
@@ -24,39 +40,37 @@ function makeItalic() {
 }
 
 // ----------------------------- Make Underline ------------------------------
-document.querySelector('.makeUnderline').addEventListener("click",makeUnderline);
+document.querySelector('.makeUnderline').addEventListener("click", makeUnderline);
 
 function makeUnderline() {
     showResult.classList.toggle('makeUnderline')
 }
 
 // ----------------------------- Make Strike ------------------------------
-document.querySelector('.makeStrike').addEventListener("click",makeUnderline);
+document.querySelector('.makeStrike').addEventListener("click", makeUnderline);
 
 function makeUnderline() {
     showResult.classList.toggle('makeStrike')
 }
 
 // ----------------------------- On Left ------------------------------
-document.querySelector('.onLeft').addEventListener("click",onLeft);
+document.querySelector('.onLeft').addEventListener("click", onLeft);
 
 function onLeft() {
     showResult.classList.toggle('onLeft')
-    console.log('click left');
 }
 
 // ----------------------------- On Right ------------------------------
-document.querySelector('.onRight').addEventListener("click",onRight);
+document.querySelector('.onRight').addEventListener("click", onRight);
 
 function onRight() {
     showResult.classList.toggle('onRight');
-    console.log('click right');
 }
 
 // ----------------------------- Clear both the Container ------------------------------
-document.querySelector('.removeContent').addEventListener("click",removeContent);
+document.querySelector('.removeContent').addEventListener("click", removeContent);
 
 function removeContent() {
-    userInput.value="";
-    showResult.innerHTML="";
+    userInput.value = "";
+    showResult.innerHTML = "";
 }
